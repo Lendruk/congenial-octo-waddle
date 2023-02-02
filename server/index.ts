61e36cc16ea086deb710b7bae5958d052f46d4bf
Lendruk <fenixeye3@gmail.com>
2023-02-02 09:51:02 +0000

    WIP: add docker support

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,7 @@ server.register(fastifyTRPCPlugin, {
 });
 (async () => {
   try {
-    await server.listen({ port: 3000 });
+    await server.listen({ port: 3000, host: "0.0.0.0" });
     console.log("Server now listening on port 3000");
   } catch (err) {
     server.log.error(err);
